1ced57d8d33a7b13b4ac518ce6840dab70628f80
oops-aman <b200054@nitsikkim.ac.in>
2022-01-18 23:38:50 +0530

    StringStream

---
+++ prob_10.cpp
@@ -0,0 +1,28 @@
+#include <sstream>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+vector<int> parseInts(string str) {
+	
+    vector<int> integers;
+    stringstream ss(str);
+    int temp;
+    char ch;
+    while(ss >> temp) {
+        integers.push_back(temp);
+        ss >> ch;
+    }
+    return integers;
+}
+
+int main() {
+    string str;
+    cin >> str;
+    vector<int> integers = parseInts(str);
+    for(int i = 0; i < integers.size(); i++) {
+        cout << integers[i] << "\n";
+    }
+    
+    return 0;
+}
